0956638eb5ad7c23e1152549351231efcb2baa03
Saad <saad8398@gmail.com>
2024-02-09 09:15:16 +0500

    implementing highscore logic

--- script.js
+++ script.js
@@ -21,9 +21,11 @@ let secretNumber= Math.trunc(Math.random()*20)+1;
 
 let score = 20;
 
+let highScore=0;
+
 document.querySelector('.again').addEventListener('click',function(){
     score=20;
-    secretNumber=secretNumber;
+    secretNumber= Math.trunc(Math.random()*20)+1;//mistake made 
     document.querySelector('.message').textContent='Start guessing...';
     document.querySelector('.number').textContent='?';
     document.querySelector('.score').textContent=score;
@@ -49,6 +51,13 @@ document.querySelector('.check').addEventListener('click',function()
         document.querySelector('body').style.backgroundColor='#60b347';
         document.querySelector('.number').style.width='30rem';  
         document.querySelector('.number').textContent=secretNumber;
+        if(score>highScore)
+        {
+            highScore=score;
+            document.querySelector('.highscore').textContent=highScore;
+        }
+    
+    
     }//When Player Wins
 
     else if(guess>secretNumber)
